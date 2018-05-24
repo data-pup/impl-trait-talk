d0125928c10bd1a9c9501336f0b2b4b156dfb080
data-pup <kevinmartinucr@gmail.com>
2018-05-24 10:11:11 -0400

    Formatting.

--- example-01-impl-iter-demonstration/src/main.rs
+++ example-01-impl-iter-demonstration/src/main.rs
@@ -2,9 +2,7 @@
 /// of the vector `v` that are greater than 5.
 fn get_impl_iter(v: Vec<u32>) -> impl Iterator<Item = u32> {
     let filter_condition = get_filter_cond(5);
-    v.into_iter()
-      .map(|i| i * i)
-      .filter(filter_condition)
+    v.into_iter().map(|i| i * i).filter(filter_condition)
 }
 
 /// Create a closure that returns true if the input is a reference
@@ -12,23 +10,23 @@ fn get_impl_iter(v: Vec<u32>) -> impl Iterator<Item = u32> {
 ///
 /// The `move` keyword forces the closure to take ownership of `min`.
 fn get_filter_cond(min: u32) -> impl Fn(&u32) -> bool {
-  move |&i| i > min
+    move |&i| i > min
 }
 
 fn main() {
-  let items = vec![1, 2, 3, 4, 5];
-  let mut results = get_impl_iter(items);
+    let items = vec![1, 2, 3, 4, 5];
+    let mut results = get_impl_iter(items);
 
-  let x_should_be_even = true;
+    let x_should_be_even = true;
 
-  let x = if x_should_be_even {
-    results.filter(|i| i % 2 == 0).next()
-  } else {
-    results.next()
-  };
+    let x = if x_should_be_even {
+        results.filter(|i| i % 2 == 0).next()
+    } else {
+        results.next()
+    };
 
-  match x {
-    Some(i) => println!("The first result is: {}", i),
-    None => println!("No results found!"),
-  }
+    match x {
+        Some(i) => println!("The first result is: {}", i),
+        None => println!("No results found!"),
+    }
 }
