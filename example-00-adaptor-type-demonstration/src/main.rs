8565b90856a72dbec5aa2cabaf8d8f011e0ac5cd
data-pup <kevinmartinucr@gmail.com>
2018-05-24 10:10:54 -0400

    Formatting.

--- example-00-adaptor-type-demonstration/src/main.rs
+++ example-00-adaptor-type-demonstration/src/main.rs
@@ -3,11 +3,11 @@
 /// functions like `map` and `filter` return.
 fn broken_function() {
     vec![1, 2, 3, 4, 5]
-      .into_iter()
-      .map(|i| i * 2)
-      .filter(|i| *i > 5)
+        .into_iter()
+        .map(|i| i * 2)
+        .filter(|i| *i > 5)
 }
 
 fn main() {
-  broken_function();
+    broken_function();
 }
