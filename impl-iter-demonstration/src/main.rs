1d6950117169aa925ec177bd802014e71c71b2de
data-pup <kevinmartinucr@gmail.com>
2018-05-24 09:58:57 -0400

    Refined the impl trait example.

--- impl-iter-demonstration/src/main.rs
+++ impl-iter-demonstration/src/main.rs
@@ -1,4 +1,4 @@
-fn get_iter(v: Vec<u32>) -> impl Iterator<Item = u32> {
+fn get_impl_iter(v: Vec<u32>) -> impl Iterator<Item = u32> {
     v.into_iter()
       .map(|i| i * i)
       .filter(|i| *i > 5)
@@ -6,12 +6,18 @@ fn get_iter(v: Vec<u32>) -> impl Iterator<Item = u32> {
 
 fn main() {
   let items = vec![1, 2, 3, 4, 5];
+  let mut results = get_impl_iter(items);
 
-  let x = get_iter(items).into_iter().nth(0);
+  let x_should_be_even = true;
+
+  let x = if x_should_be_even {
+    results.filter(|i| i % 2 == 0).next()
+  } else {
+    results.next()
+  };
 
   match x {
-    Some(i) => println!("The first squared value > 5 is: {}", i),
-    None => println!("None of the items' square > 5"),
+    Some(i) => println!("The first result is: {}", i),
+    None => println!("No results found!"),
   }
 }
-
